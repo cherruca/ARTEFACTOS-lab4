640176b277de8928420f98fd74174c37a16cc62f
cherruca <00402523@uca.edu.sv>
2026-09-25 17:24:44 -0600

    ADD: primer prueba con el touch

---
+++ 01-primer-touch.ino
@@ -0,0 +1,19 @@
+const int TOUCH_PIN = 4;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  delay(1000);
+
+  Serial.println("PRUEBA SENSOR TOUCH");
+  Serial.println();
+}
+
+void loop() {
+
+  int valorTouch = touchRead(TOUCH_PIN);
+  Serial.print("IVAN NOLASCO 00402523 Valor Touch: ");
+  Serial.println(valorTouch);
+  delay(200);
+}
